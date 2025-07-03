f8d2a9303e8cb364ae9260c2a1fe27258947b8d3
Ivan Derlich <a@ivanderlich.com>
2025-07-03 15:24:01 -0300

    Add remove user method in service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -34,6 +34,12 @@ export class UsersService {
     return this.repo.save(user)
   }
 
-  remove(){}
+  async remove(id: number){
+    const user = await this.repo.findOneBy({id})
+    if (!user) {
+      throw new NotFoundException('user not found')
+    }
+    return this.repo.remove(user)
+  }
 
 }
