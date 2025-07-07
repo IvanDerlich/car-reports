4de47c69d9ce18052f18ac4e37e2275b5fdad201
Ivan Derlich <a@ivanderlich.com>
2025-07-07 00:09:38 -0300

    Add types to serialization

--- src/interceptors/seralized.interceptor.ts
+++ src/interceptors/seralized.interceptor.ts
@@ -7,9 +7,14 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 import { nextTick } from 'process';
+import { UserDto } from 'src/users/dtos/user.dto';
 
+type DtoType = typeof UserDto
 
-export function Serialize(dto: any) {
+// List all the DtoTypes that you wont to use in the interceptor
+type DtoTypes = DtoType
+
+export function Serialize(dto: DtoTypes) {
   return UseInterceptors(new SerializedInterceptor(dto));
 }
 
@@ -19,7 +24,7 @@ class SerializedInterceptor implements NestInterceptor {
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // console.log('I am running before the handler', context);
-    return handler.handle().pipe(map((data: any) => {
+    return handler.handle().pipe(map((data: DtoTypes) => {
       //console.log("I am running after the handler", data)
       return plainToClass(this.dto, data, {
         excludeExtraneousValues: true,
