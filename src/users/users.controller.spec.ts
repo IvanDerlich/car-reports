0dc7a4786ea1f0c357b99a6e5e9688340e31c086
Ivan Derlich <a@ivanderlich.com>
2025-07-22 19:14:34 -0300

    Add test coverage and reformat test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    const user: User = new User();
+    user.email = 'test@test.com';
+    user.password = '123456';
+
+    fakeAuthService = {
+      signup: (email: string, password: string) => Promise.resolve(user),
+      signin: (email: string, password: string) => Promise.resolve(user),
+    };
+
+    fakeUsersService = {
+      find: (email: string) =>
+        Promise.resolve([user].filter((user) => user.email === email)),
+      findOneById: (id: number) => Promise.resolve(user),
+      remove: (id: number) => Promise.resolve(user),
+      update: (id: number, attrs: Partial<User>) => Promise.resolve(user),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
