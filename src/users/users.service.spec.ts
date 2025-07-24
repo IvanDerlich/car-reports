4aa49f7921e4372e876198610417550a8be92939
Ivan Derlich <a@ivanderlich.com>
2025-07-24 17:10:58 -0300

    Test users service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let fakeRepo: any;
 
   beforeEach(async () => {
+    const user: User = new User();
+    user.id = 1;
+    user.email = 'test@test.com';
+    user.password = '123456';
+
+    fakeRepo = {
+      create: jest.fn().mockReturnValue(user),
+      save: jest.fn().mockResolvedValue(user),
+      findOneBy: jest.fn().mockResolvedValue(user),
+      find: jest.fn().mockResolvedValue([user]),
+      remove: jest.fn().mockResolvedValue(user),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: fakeRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
