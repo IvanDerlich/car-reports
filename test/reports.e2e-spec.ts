b9c92c707e2e49cabdfa71bc4beb60537c84cbfe
Ivan Derlich <a@ivanderlich.com>
2025-07-28 15:05:27 -0300

    Create a file with comments to document future tests

---
+++ test/reports.e2e-spec.ts
@@ -0,0 +1,11 @@
+// Try to create a report while being logged in
+
+// Try to create a report without being logged in
+
+// Try to get all reports
+
+// Try to get a report by id
+
+// Try to update a report
+
+// Try to delete a report
