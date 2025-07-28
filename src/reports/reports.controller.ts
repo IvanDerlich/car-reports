297959c37927c6b32cf1a2c3a2006e63f4907c88
Ivan Derlich <a@ivanderlich.com>
2025-07-28 15:03:53 -0300

    Populate with code the create report method in the controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthService } from 'src/users/auth.service';
+import { AuthGuard } from 'src/users/guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(
+    private reportsService: ReportsService,
+    private authService: AuthService,
+  ) {
+    this.reportsService = reportsService;
+    this.authService = authService;
+  }
+
   @Post()
+  @UseGuards(AuthGuard)
   createReport(@Body() body: CreateReportDto) {
-    console.log(body);
-    return 'report created';
+    return this.reportsService.create(body);
   }
 }
