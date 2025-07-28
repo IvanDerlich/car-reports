49b244453429ed0f3cc83481616215f502ae0547
Ivan Derlich <a@ivanderlich.com>
2025-07-28 15:04:36 -0300

    Add all the necessary dependencies in the dependency injector

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -3,10 +3,16 @@ import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
+import { AuthService } from 'src/users/auth.service';
+import { UsersService } from '@/users/users.service';
+import { User } from '@/users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report])],
+  imports: [
+    TypeOrmModule.forFeature([Report]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, AuthService, UsersService],
 })
 export class ReportsModule {}
