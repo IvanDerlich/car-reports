ff23a60febff1adc7ba95a2e631b358a82979df7
Ivan Derlich <a@ivanderlich.com>
2025-07-27 23:16:38 -0300

    Add report entity

--- src/reports/reports.entity.ts
+++ src/reports/reports.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string; // Company that made the car: Ferrari, Toyota, etc.
+
+  @Column()
+  model: string; // Corolla, Camry, etc.
+
+  @Column()
+  year: number; // Year the car was manufactured
+
+  @Column()
+  lng: number; // Longitude
+
+  @Column()
+  lat: number; // Latitude
+
+  @Column()
+  mileage: number; // How many miles the car has been driven
 }
