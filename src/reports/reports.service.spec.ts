ecf05ea702ab95f2e83f01a7bca5555ad1c09079
Ivan Derlich <a@ivanderlich.com>
2025-07-29 18:49:54 -0300

    Mock report repository in report service

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsService } from './reports.service';
+import { Report } from './reports.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ReportsService', () => {
   let service: ReportsService;
+  let fakeReportsRepo: Partial<ReportsService>;
 
   beforeEach(async () => {
+    fakeReportsRepo = {
+      create: jest.fn().mockResolvedValue(new Report()),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportsService],
+      providers: [
+        ReportsService,
+        { provide: getRepositoryToken(Report), useValue: fakeReportsRepo },
+      ],
     }).compile();
 
     service = module.get<ReportsService>(ReportsService);
