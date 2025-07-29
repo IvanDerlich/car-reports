86b5524d6655ecd380146ed4f67c4b8efa33c82c
Ivan Derlich <a@ivanderlich.com>
2025-07-29 18:41:29 -0300

    Mock service in controller test

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let fakeReportsService: Partial<ReportsService>;
 
   beforeEach(async () => {
+    fakeReportsService = {
+      create: jest.fn(),
+      // changeApproval: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
+      providers: [
+        {
+          provide: ReportsService,
+          useValue: fakeReportsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
