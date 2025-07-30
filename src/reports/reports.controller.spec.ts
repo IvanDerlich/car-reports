b7b4bd4d8075fe192b2a858d0739722ff0ac2ba4
Ivan Derlich <a@ivanderlich.com>
2025-07-29 22:18:38 -0300

    Mock auth service in reports controllers

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { AuthService } from '@/users/auth.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
   let fakeReportsService: Partial<ReportsService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
     fakeReportsService = {
       create: jest.fn(),
       // changeApproval: jest.fn(),
+    };
+
+    fakeAuthService = {
+      signup: jest.fn(),
+      signin: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -18,6 +25,10 @@ describe('ReportsController', () => {
         {
           provide: ReportsService,
           useValue: fakeReportsService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
         },
       ],
     }).compile();
