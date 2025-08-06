504831f0fb90de5ad5ded30561c044b2c44e21aa
Ivan Derlich <a@ivanderlich.com>
2025-08-06 00:18:37 -0300

    Improve factories

--- dev/db/factories/reports.factory.ts
+++ dev/db/factories/reports.factory.ts
@@ -1,44 +1,53 @@
-import { setSeederFactory } from 'typeorm-extension';
 import { Report } from '../../../src/reports/reports.entity';
+import { User } from '../../../src/users/user.entity';
+import { CustomReportData } from '../types';
 import { faker } from '@faker-js/faker';
 
-// Type for custom report data
-export type CustomReportData = {
-  make: string;
-  model: string;
-  year: number;
-  lng: number;
-  lat: number;
-  mileage: number;
-  approved?: boolean;
-  price: number;
-  userId: number;
-};
-
 // Custom factory function that can accept custom data
-export const CustomReportFactory = (customData: CustomReportData): Report => {
-  const report = new Report();
-  report.make = customData.make;
-  report.model = customData.model;
-  report.year = customData.year;
-  report.lng = customData.lng;
-  report.lat = customData.lat;
-  report.mileage = customData.mileage;
-  report.approved = customData.approved ?? false;
-  report.price = customData.price;
-  // Note: userId will be set when saving to database
-  return report;
-};
+export class CustomReportFactory {
+  static async make(customData: CustomReportData, user: User): Promise<Report> {
+    const report = new Report();
+    report.make = customData.make;
+    report.model = customData.model;
+    report.year = customData.year;
+    report.lng = customData.lng;
+    report.lat = customData.lat;
+    report.mileage = customData.mileage;
+    report.approved = customData.approved ?? false;
+    report.price = customData.price;
+    report.user = user;
+    return report;
+  }
 
-export const RandomReportFactory = setSeederFactory(Report, () => {
-  const report = new Report();
-  report.make = faker.vehicle.manufacturer();
-  report.model = faker.vehicle.model();
-  report.year = faker.date.past().getFullYear();
-  report.lng = faker.location.longitude();
-  report.lat = faker.location.latitude();
-  report.mileage = faker.number.int({ min: 10000, max: 100000 });
-  report.approved = faker.datatype.boolean();
-  report.price = faker.number.int({ min: 10000, max: 100000 });
-  return report;
-});
+  static async makeMany(customData: CustomReportData[], users: User[]): Promise<Report[]> {
+    const reports: Report[] = [];
+    for (const data of customData) {
+      reports.push(await this.make(data, faker.helpers.arrayElement(users)));
+    }
+    return reports;
+  }
+}
+
+export class RandomReportFactory {
+  static async make(user: User): Promise<Report> {
+    const report = new Report();
+    report.make = faker.vehicle.manufacturer();
+    report.model = faker.vehicle.model();
+    report.year = faker.date.past().getFullYear();
+    report.lng = faker.location.longitude();
+    report.lat = faker.location.latitude();
+    report.mileage = faker.number.int({ min: 10000, max: 100000 });
+    report.approved = faker.datatype.boolean();
+    report.price = faker.number.int({ min: 10000, max: 100000 });
+    report.user = user;
+    return report;
+  }
+
+  static async makeMany(count: number, users: User[]): Promise<Report[]> {
+    const reports: Report[] = [];
+    for (let i = 0; i < count; i++) {
+      reports.push(await this.make(faker.helpers.arrayElement(users)));
+    }
+    return reports;
+  }
+}
--- dev/db/factories/users.factory.ts
+++ dev/db/factories/users.factory.ts
@@ -1,31 +1,42 @@
-import { setSeederFactory } from 'typeorm-extension';
 import { User } from '../../../src/users/user.entity';
 import { hash } from 'argon2';
+import { CustomUserData } from '../types';
 import { faker } from '@faker-js/faker';
 
-// Type for custom user data
-export type CustomUserData = {
-  email: string;
-  password: string;
-  admin?: boolean;
-};
+export class CustomUserFactory {
+  static async make(customData: CustomUserData): Promise<User> {
+    const hashedPassword = await hash(customData.password);
+    const user = new User();
+    user.email = customData.email;
+    user.password = hashedPassword;
+    user.admin = customData.admin ?? false;
+    return user;
+  }
 
-// Custom factory function that can accept custom data
-export const CustomUserFactory = async (
-  customData: CustomUserData,
-): Promise<User> => {
-  const hashedPassword = await hash(customData.password);
-  const user = new User();
-  user.email = customData.email;
-  user.password = hashedPassword;
-  user.admin = customData.admin ?? false;
-  return user;
-};
+  static async makeMany(customData: CustomUserData[]): Promise<User[]> {
+    const users: User[] = [];
+    for (const data of customData) {
+      users.push(await this.make(data));
+    }
+    return users;
+  }
+}
 
-export const RandomUserFactory = setSeederFactory(User, async () => {
-  const hashedPassword = await hash(faker.internet.password());
-  const user = new User();
-  user.email = faker.internet.email();
-  user.password = hashedPassword;
-  return user;
-});
+
+
+export class RandomUserFactory {
+  static async make(): Promise<User> {
+    const user = new User();
+    user.email = faker.internet.email();
+    user.password = await hash(faker.internet.password());
+    return user;
+  }
+
+  static async makeMany(count: number): Promise<User[]> {
+    const users: User[] = [];
+    for (let i = 0; i < count; i++) {
+      users.push(await this.make());
+    }
+    return users;
+  }
+}
