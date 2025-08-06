4ccf4e4533c62743ef2d14d4ba828d73a961bfd3
Ivan Derlich <a@ivanderlich.com>
2025-08-06 00:20:25 -0300

    Improve seeder

--- dev/db/seed.ts
+++ dev/db/seed.ts
@@ -1,23 +1,42 @@
 import { DataSource } from 'typeorm';
 import { User } from '../../src/users/user.entity';
-import { hash } from 'argon2';
-import { faker } from '@faker-js/faker';
+import { Report } from '../../src/reports/reports.entity';
+import {
+  RandomReportFactory,
+  RandomUserFactory,
+  CustomUserFactory,
+  CustomReportFactory,
+} from './factories';
+import { usersFixtures } from './fixtures/users';
+import { reportsFixtures } from './fixtures/reports';
 
 // Seeder function that requires a DataSource
 export const seedDatabase = async (dataSource: DataSource) => {
-  // Create 10 random users
-  const users: User[] = [];
-  for (let i = 0; i < 10; i++) {
-    const hashedPassword = await hash(faker.internet.password());
-    const user = new User();
-    user.email = faker.internet.email();
-    user.password = hashedPassword;
-    user.admin = faker.datatype.boolean();
-    users.push(user);
-  }
+
+  // Create 10 random users with random user factory
+  const randomUsers = await RandomUserFactory.makeMany(10);
+
+  // Create 10 custom users
+  const customUsers = await CustomUserFactory.makeMany(usersFixtures);
+
+  const users = [...randomUsers, ...customUsers];
 
   // Save users to database
   await dataSource.getRepository(User).save(users);
-
   console.log(`Created ${users.length} random users`);
+
+  // Create 10 reports with random data
+  const randomReports: Report[] = await RandomReportFactory.makeMany(10, users);
+
+  // Create 10 reports with custom data
+  const customReports: Report[] = await CustomReportFactory.makeMany(
+    reportsFixtures,
+    users,
+  );
+
+  const reports: Report[] = [...randomReports, ...customReports];
+
+  // Save reports to database
+  await dataSource.getRepository(Report).save(reports);
+  console.log(`Created ${reports.length} random reports`);
 };
