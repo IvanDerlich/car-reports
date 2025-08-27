9277cc5bdea80d3bfea086f018d147bf05d64ed0
Ivan Derlich <a@ivanderlich.com>
2025-08-27 14:20:42 -0300

    Allow method level override of the interceptor

--- src/interceptors/seralized.interceptor.ts
+++ src/interceptors/seralized.interceptor.ts
@@ -3,25 +3,45 @@ import type { CallHandler, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
+import { Reflector } from '@nestjs/core';
 import { UserDto } from '@/users/dtos/user.dto';
 import { ReportDto } from '@/reports/dtos/report.dto';
 import { GetEstimateReturnValueDto } from '@/reports/dtos/get-estimate-return-value.dto';
+import { MessageDto } from '@/dtos/message.dto';
 
-type ApprovedDtos = UserDto | ReportDto | GetEstimateReturnValueDto;
+type ApprovedDtos =
+  | UserDto
+  | ReportDto
+  | GetEstimateReturnValueDto
+  | MessageDto;
+
+const SERIALIZE_DTO_KEY = 'serialize_dto';
 
 export function Serialize<T extends ApprovedDtos>(dto: new () => T) {
   return UseInterceptors(new SerializedInterceptor(dto));
+}
+
+// New decorator for method-level overrides
+export function SerializeResponse<T extends ApprovedDtos>(dto: new () => T) {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    Reflect.defineMetadata(SERIALIZE_DTO_KEY, dto, descriptor.value);
+    return descriptor;
+  };
 }
 
 class SerializedInterceptor<T extends ApprovedDtos> implements NestInterceptor {
   constructor(private dto: new () => T) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // console.log('I am running before the handler', context);
+    const reflector = new Reflector();
+    const methodDto = reflector.get(SERIALIZE_DTO_KEY, context.getHandler());
+
+    // Use method-level DTO if available, otherwise use class-level DTO
+    const dtoToUse = methodDto || this.dto;
+
     return handler.handle().pipe(
       map((data: any) => {
-        //console.log("I am running after the handler", data)
-        return plainToClass(this.dto, data, {
+        return plainToClass(dtoToUse, data, {
           excludeExtraneousValues: true,
         });
       }),
